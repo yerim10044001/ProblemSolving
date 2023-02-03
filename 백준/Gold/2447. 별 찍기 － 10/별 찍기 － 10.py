7b16abb4f5ac6ccf49cae6a4babbd3dadc4b08ee
Ye Rim Lee <86493874+yerim10044001@users.noreply.github.com>
2023-02-03 12:05:52 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 3328 ms, Memory: 69684 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    n = int(input())
+
+    starList = [['*' for _ in range(0, n)] for _ in range(0, n)]
+
+    k = 3
+    while k <= n:
+        for i in range(0, n, k):
+            for j in range(0, n, k):
+                for h in range(k//3, k//3+k//3):
+                    for g in range(k//3, k//3+k//3):
+                        starList[i+h][j+g] = ' '
+
+        k *= 3
+
+    # print star
+    for i in range(0, n):
+        for j in range(0, n):
+            print(starList[i][j], end = '')
+        print()
