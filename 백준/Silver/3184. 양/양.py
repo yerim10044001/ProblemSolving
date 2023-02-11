e42f3dbc639ad928de69e6d9bad063b50402e0a3
Ye Rim Lee <86493874+yerim10044001@users.noreply.github.com>
2023-02-11 11:19:54 +0900

    [Silver I] Title: 양, Time: 112 ms, Memory: 34216 KB -BaekjoonHub

--- 백준/Silver/3184. 양/양.py
+++ 백준/Silver/3184. 양/양.py
@@ -2,24 +2,40 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
-def BFS(startNode):
+def BFS(i, j):
     que = deque()
-    que.append(startNode)
-    visited[startNode[0]][startNode[1]] = True
-    o, v = 0, 0     # 양, 늑대
+    que.append((i, j))
+    visited[i][j] = True
+
+    o, v = 0, 0 # 양, 늑대
 
     while que:
-        node = que.popleft()
-        if field[node[0]][node[1]] == 'o':    # 양
+        i, j = que.popleft()
+        
+        # sheep or wolf
+        if field[i][j] == 'o':
             o += 1
-        elif field[node[0]][node[1]] == 'v':  # 늑대
+        elif field[i][j] == 'v':
             v += 1
 
-        for w in graph[node]:
-            if not visited[w[0]][w[1]]:
-                visited[w[0]][w[1]] = True
-                que.append(w)
-
+        # next point
+        # left
+        if i != 0 and not visited[i-1][j] and field[i-1][j] != '#':
+            visited[i-1][j] = True
+            que.append((i-1, j))
+        # right
+        if i != r-1 and not visited[i+1][j] and field[i+1][j] != '#':
+            visited[i+1][j] = True
+            que.append((i+1, j))
+        # up
+        if j != 0 and not visited[i][j-1] and field[i][j-1] != '#':
+            visited[i][j-1] = True
+            que.append((i, j-1))
+        # down
+        if j != c-1 and not visited[i][j+1] and field[i][j+1] != '#':
+            visited[i][j+1] = True
+            que.append((i, j+1))
+    
     if o > v:
         return o, 0
     else:
@@ -29,31 +45,18 @@ if __name__ == "__main__":
     r, c = map(int, input().split())
 
     field = []
+    visited = []
     for _ in range(r):
         field.append(list(input().rstrip()))
+        visited.append([False]*c)
 
-    # make graph
-    graph = {}
-    for i in range(0, r):
-        for j in range(0, c):
-            graph[(i, j)] = []
-            if i != 0 and field[i][j] != '#' and field[i-1][j] != '#':
-                graph[(i-1, j)].append((i, j))
-                graph[(i, j)].append((i-1, j))
-            if j != 0 and field[i][j] != '#' and field[i][j-1] != '#':
-                graph[(i, j-1)].append((i, j))
-                graph[(i, j)].append((i, j-1))
-    
     # BFS
-    visited = [[False for _ in range(c)] for _ in range(r)]
-
-    Oanswer, Vanswer = 0, 0
-
-    for i in range(0, r):
-        for j in range(0, c):
+    sheep, wolf = 0, 0
+    for i in range(r):
+        for j in range(c):
             if not visited[i][j] and field[i][j] != '#':
-                o, v = BFS((i, j))
-                Oanswer += o
-                Vanswer += v
-    
-    print(Oanswer, Vanswer)
+                o, v = BFS(i, j)
+                sheep += o
+                wolf += v
+
+    print(sheep, wolf)
