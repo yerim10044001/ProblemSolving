9b3395958b78257cb9a909a27b3a92979dae1a3a
Ye Rim Lee <86493874+yerim10044001@users.noreply.github.com>
2023-02-19 12:26:32 +0900

    [Silver I] Title: RGB거리, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    n = int(input())
+
+    house = []
+    for _ in range(n):
+        house.append(list(map(int, input().split())))
+
+    for i in range(1, n):
+        house[i][0] += min(house[i-1][1], house[i-1][2])
+        house[i][1] += min(house[i-1][0], house[i-1][2])
+        house[i][2] += min(house[i-1][0], house[i-1][1])
+       
+    print(min(house[n-1]))
