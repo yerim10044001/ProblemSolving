bf170becceebb917d1fa0a7aeb89826e9729e408
Ye Rim Lee <86493874+yerim10044001@users.noreply.github.com>
2023-03-06 23:06:53 +0900

    1654. 랜선자르기

---
+++ 백준/Silver/1654. 랜선자르기/1654.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    k, n = map(int, input().split())
+
+    lines = []
+    for _ in range(k):
+        lines.append(int(input()))
+
+    left = 1
+    right = max(lines)
+
+    result = 1
+    while left <= right:
+        s = (left+right)//2
+
+        cnt = 0
+        for i in lines:
+            cnt += i//s
+        
+        # check s is answer
+        if cnt < n:
+            right = s-1
+        else:
+            left = s+1
+            result = max(result, s)
+
+    print(result)
