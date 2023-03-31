23e58c03a7306b6543a4e63f9e3c31b1f705c996
Ye Rim Lee <86493874+yerim10044001@users.noreply.github.com>
2023-03-31 17:00:59 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,40 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-def getNumber(expression, i):
-    number = []
-    while  i < len(expression) and expression[i] != '+' and expression[i] != '-':
-        number.append(expression[i])
-        i += 1
-    number = int(''.join(number))
-    return number, i
-
 if __name__ == "__main__":
     expression = input().rstrip()
+    try:
+        m = expression.index('-')
+        answer = sum(list(map(int, expression[:m].split('+'))))
+        expression = expression[m+1:].replace('-','+')
+        answer -= sum(list(map(int, expression.split('+'))))
 
-    answer, i = 0, 0
-
-    # first number always 'plus'
-    number, i = getNumber(expression, i)
-    answer += number
-
-    # () start
-    p = True
-
-    while i < len(expression):
-        # plus
-        if p and expression[i] == '+': p = True
-        # minus
-        elif expression[i] == '-': p = False
-        i += 1
-
-        # in ( )
-        if not p:
-            number, i = getNumber(expression, i)
-            answer -= number
-        # not in ()
-        else:
-            number, i = getNumber(expression, i)
-            answer += number
+    except:
+        answer = sum(list(map(int, expression.split('+'))))
 
     print(answer)
