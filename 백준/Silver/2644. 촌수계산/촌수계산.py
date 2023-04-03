cfb11b9f6d1d29eb969c61db91ff761e6338616b
Ye Rim Lee <86493874+yerim10044001@users.noreply.github.com>
2023-04-03 14:51:55 +0900

    [Silver II] Title: 촌수계산, Time: 76 ms, Memory: 34144 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.py
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -2,37 +2,32 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-def bfs(start, end):
-    q = deque()
-    q.append((start, 0))
-    visited = [False]*(n+1)
-    visited[start] = True
+def dfs(node, cnt):
+    if node == y:
+        global result
+        result = cnt
     
-    while q:
-        node, cnt = q.popleft()
+    for v in graph[node]:
+        if not visited[v]:
+            visited[v] = True
+            dfs(v, cnt+1)
 
-        if node == end:
-            return cnt
-
-        for v in graph[node]:
-            if not visited[v]:
-                visited[v] = True
-                q.append((v, cnt+1))
-
-    return -1
 
 if __name__ == "__main__":
     n = int(input())
     x, y = map(int, input().split())
     m = int(input())
 
-    graph = {}
-    for i in range(1, n+1):
-        graph[i] = []
+    graph = [[] for _ in range(n+1)]
 
     for _ in range(m):
         p, s = map(int, input().split())
         graph[p].append(s)
         graph[s].append(p)
 
-    print(bfs(x, y))    
+    visited = [False] * (n+1)
+    visited[x] = True
+    result = -1
+    dfs(x, 0)
+
+    print(result)
