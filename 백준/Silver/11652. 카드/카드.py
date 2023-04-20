0e2093f2176ebc2371574396df00974dff68da6f
Ye Rim Lee <86493874+yerim10044001@users.noreply.github.com>
2023-04-20 18:01:34 +0900

    [Silver IV] Title: 카드, Time: 160 ms, Memory: 43036 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.py
+++ 백준/Silver/11652. 카드/카드.py
@@ -4,19 +4,14 @@ input = sys.stdin.readline
 
 if __name__ == "__main__":
     n = int(input())
-    maxNum = 0
-    maxCnt = 0
-    numList = {}
+    numDic = {}
     for _ in range(n):
         num = int(input())
         try:
-            numList[num] += 1
+            numDic[num] += 1
         except:
-            numList[num] = 1
-        if numList[num] == maxCnt:
-                maxNum = min(num, maxNum)
-        elif numList[num] > maxCnt:
-            maxNum = num
-            maxCnt = numList[num]
-    
-    print(maxNum)
+            numDic[num] = 1
+
+    ans = max(numDic.values())
+    ans = min(k for k, v in numDic.items() if v == ans)
+    print(ans)
