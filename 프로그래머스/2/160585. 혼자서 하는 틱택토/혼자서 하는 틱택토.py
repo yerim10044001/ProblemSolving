405a8366f077c43a233ad951f4cdceb45fe9b6e8
Yerim Lee <86493874+yerim10044001@users.noreply.github.com>
2024-02-08 21:09:49 +0900

    [level 2] Title: 혼자서 하는 틱택토, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.py
@@ -0,0 +1,47 @@
+def solution(board):
+    # 선공과 후공의 개수 비교
+    o_cnt, x_cnt = 0, 0
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == "O":
+                o_cnt += 1
+            elif board[i][j] == "X":
+                x_cnt += 1
+    if o_cnt == x_cnt or o_cnt-x_cnt == 1:
+        pass
+    else:
+        return 0
+    
+    # 이미 게임이 종료되었음에도 게임을 진행하였는지 확인
+    o_win = False
+    x_win = False
+    # 가로
+    for i in range(3):
+        if board[i] == "OOO":
+            o_win = True
+        if board[i] == "XXX":
+            x_win = True
+    # 세로
+    for i in range(3):
+        if board[0][i]+board[1][i]+board[2][i] == "OOO":
+            o_win = True
+        if board[0][i]+board[1][i]+board[2][i] == "XXX":
+            x_win = True
+    # 대각선
+    if board[0][0]+board[1][1]+board[2][2] == "OOO":
+        o_win = True
+    if board[0][0]+board[1][1]+board[2][2] == "XXX":
+        x_win = True
+    if board[0][2]+board[1][1]+board[2][0] == "OOO":
+        o_win = True
+    if board[0][2]+board[1][1]+board[2][0] == "XXX":
+        x_win = True
+    
+    if o_win and x_win:
+        return 0
+    if o_win and o_cnt != x_cnt+1:
+        return 0
+    if x_win and o_cnt != x_cnt:
+        return 0
+
+    return 1
