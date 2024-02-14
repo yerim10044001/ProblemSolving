4b2ee3d9fb035d20c637180e66c2e861951da7f6
Yerim Lee <86493874+yerim10044001@users.noreply.github.com>
2024-02-14 22:39:48 +0900

    [level 2] Title: 미로 탈출, Time: 0.04 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -8,49 +8,41 @@ def solution(maps):
         for j in range(m):
             if maps[i][j] == 'S':
                 start = (i, j)   
-
+            elif maps[i][j] == 'L':
+                lever = (i, j)
+            elif maps[i][j] == 'E':
+                exit = (i, j)
+                
+    # bfs
+    def bfs(start, end):
+        answer = 0
+        q = deque()
+        q.append((start[0], start[1], 0))
+        visited = set()
+        visited.add(start)
+        while q:
+            x, y, cnt = q.popleft()
+            if x == end[0] and y == end[1]:
+                return cnt
+            for i, j in ((-1, 0), (0, -1), (1, 0), (0, 1)):
+                next_x = x+i
+                next_y = y+j
+                if 0 <= next_x < n and 0 <= next_y < m:
+                    if (next_x, next_y) not in visited and maps[next_x][next_y] != 'X':
+                        visited.add((next_x, next_y))
+                        q.append((next_x, next_y, cnt+1))
+        return -1
+    
     # bfs, find lever
-    q = deque()
-    q.append((start[0], start[1], 0))
-    visited = set()
-    visited.add(start)
-    lever = (-1, -1)
-    while q:
-        x, y, cnt = q.popleft()
-        if maps[x][y] == 'L':
-            lever = (x, y)
-            answer += cnt
-            break
-        for i, j in ((-1, 0), (0, -1), (1, 0), (0, 1)):
-            next_x = x+i
-            next_y = y+j
-            if 0 <= next_x < n and 0 <= next_y < m:
-                if (next_x, next_y) not in visited and maps[next_x][next_y] != 'X':
-                    visited.add((next_x, next_y))
-                    q.append((next_x, next_y, cnt+1))
-    if lever[0] == -1:
+    lever_cnt = bfs(start, lever)
+    if lever_cnt == -1:
         return -1
-            
+    else: answer += lever_cnt
+    
     # bfs, find exit
-    q = deque()
-    q.append((lever[0], lever[1], 0))
-    visited = set()
-    visited.add(lever)
-    exit = (-1, -1)
-    while q:
-        x, y, cnt = q.popleft()
-        if maps[x][y] == 'E':
-            exit = (x, y)
-            answer += cnt
-            break
-        for i, j in ((-1, 0), (0, -1), (1, 0), (0, 1)):
-            next_x = x+i
-            next_y = y+j
-            if 0 <= next_x < n and 0 <= next_y < m:
-                if (next_x, next_y) not in visited and maps[next_x][next_y] != 'X':
-                    visited.add((next_x, next_y))
-                    q.append((next_x, next_y, cnt+1))
-    if exit[0] == -1:
+    exit_cnt = bfs(lever, exit)
+    if exit_cnt == -1:
         return -1
+    else: answer += exit_cnt
     
     return answer
