75633abddc35b5445da11c6245c0f87ae4c702fc
Yerim Lee <86493874+yerim10044001@users.noreply.github.com>
2024-04-08 17:48:43 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+def solution(weights):
+    answer, counter = 0, Counter(weights)
+
+    for i in counter:
+        answer += counter[i] * (counter[i] - 1) // 2 # nC2 n!/2!(n - 2)!
+        answer += counter[i] * counter[i * 3 / 2]
+        answer += counter[i] * counter[i * 2]
+        answer += counter[i] * counter[i * 4 / 3]
+
+    return answer
